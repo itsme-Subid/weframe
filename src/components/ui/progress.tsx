daece7447f7c7f0eb7c353694327c86e506af88c
SUBID DAS <subid.das@awesomesuite.com>
2023-09-05 01:37:42 +0530

    Minor Fixes

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,10 +5,17 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  className?: string;
+  foreground?: string;
+  value?: number;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, value, foreground, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -21,7 +28,7 @@ const Progress = React.forwardRef<
       className={`h-full w-full flex-1 rounded-full transition-all`}
       style={{
         transform: `translateX(-${100 - (value || 0)}%)`,
-        backgroundColor: props.foreground,
+        backgroundColor: foreground,
       }}
     />
   </ProgressPrimitive.Root>
